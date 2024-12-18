7852868731344556e64ee3eb664b2a1b2c8c39f9
Filip Tatarnik <ftatarnik@gmail.com>
2024-12-18 07:16:27 -0600

    added snow effect to portfolio for winter

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import LandingPage from './components/LandingPage';
 import AboutUs from './components/AboutUs';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
+import { SnowOverlay } from 'react-snow-overlay';
 
 function App() {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
   return (
     <div className="flex flex-col min-h-screen">
+          <SnowOverlay />
+
       <Navbar isOpen={isNavbarOpen} setIsOpen={setIsNavbarOpen} />
       <div className="flex-grow pl-0 md:pl-64"> {/* Main content wrapper */}
         <main className="flex-grow">
