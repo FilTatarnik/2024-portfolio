551fd1766bd79fee34f885d78bccc3d21b3113d1
Filip Tatarnik <ftatarnik@gmail.com>
2026-06-24 12:42:37 -0500

    Changed the footer so it doesnt stack so stupidly and it looks nice now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,36 +11,33 @@ import { Triangle } from "lucide-react";
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 left-0 md:left-64 right-0 bg-blue-900 text-white py-1 sm:py-2 px-3 sm:px-6 text-xs sm:text-sm md:text-base">
-      <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
-        <div className="mb-1 sm:mb-0 text-center sm:text-left">
-          <p className="hidden sm:inline">&copy; 2026 </p>
-          <Link
-            to="home"
-            smooth={true}
-            duration={500}
-            className="text-blue-400 hover:text-blue-300 cursor-pointer transition-colors"
-          >
-            Filip Tatarnik
-          </Link>
+      <div className="container mx-auto flex flex-row justify-between items-center">
+        <Link
+          to="home"
+          smooth={true}
+          duration={500}
+          className="text-blue-400 hover:text-blue-300 cursor-pointer transition-colors"
+        >
+          <span className="hidden sm:inline">&copy; 2026 </span>Filip Tatarnik
           <span className="hidden sm:inline"> All rights reserved.</span>
-        </div>
-        <div className="mb-1 sm:mb-0 text-center">
+        </Link>
+        <div className="flex items-center space-x-2 sm:space-x-4">
           <a
             href="/privacy-policy"
-            className="text-blue-400 hover:text-blue-300 mx-1 sm:mx-2 transition-colors"
+            className="text-blue-400 hover:text-blue-300 transition-colors"
           >
             <span className="sm:hidden">Privacy</span>
             <span className="hidden sm:inline">Privacy Policy</span>
           </a>
           <a
             href="/terms-of-service"
-            className="text-blue-400 hover:text-blue-300 mx-1 sm:mx-2 transition-colors"
+            className="text-blue-400 hover:text-blue-300 transition-colors"
           >
             <span className="sm:hidden">Terms</span>
             <span className="hidden sm:inline">Terms of Service</span>
           </a>
         </div>
-        <div className="flex justify-center sm:justify-end space-x-3 items-baseline">
+        <div className="flex items-baseline space-x-3">
           <a
             href="https://github.com/FilTatarnik"
             target="_blank"
